59e45b0e22aa24e14816546d452e8d12a02c9ce8
Aleksandra Moss <mossaleksandra@gmail.com>
2023-11-26 22:37:30 +0100

    Added CSS to header and footer.

--- PawPals/src/components/Header.jsx
+++ PawPals/src/components/Header.jsx
@@ -3,14 +3,14 @@ import "../CSS/Header.css";
 
 function Header() {
   return (
-      <nav className="Nav">
-        <header className="Header">
+      <nav className="nav">
+        <header className="header">
         <img
-          className="Logo"
+          className="logo"
           src="https://img.freepik.com/free-vector/pet-logo-design-paw-vector-animal-shop-business_53876-136741.jpg?w=900&t=st=1700557351~exp=1700557951~hmac=f2fafe342ef9bf250c93201f868ad647c5e3455d176b1c5a2530001a7322f75b"
         />
-        <h2 id="Title">Paw Pals</h2></header>
-        <ul className="NavList">
+        <span id="title">Paw Pals</span></header>
+        <ul className="navlist">
           <li>Pricing</li>
           <li>About</li>
           <li>Contact</li>
